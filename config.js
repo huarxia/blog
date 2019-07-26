1ca250fe37685edc1f156835c9c46ec4589fb7cc
花夏 <liubiao0810@live.cn>
2019-07-26 14:16:42 +0800

    feat: 更新博文

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.huar.love/blog/',
+        url: 'https://huarxia.github.io/blog/',
         mail: {},
         database: {
             client: 'sqlite3',
